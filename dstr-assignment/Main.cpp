fd87257afed119bc7fa8f2d848df794f5b866626
daniellim051000 <daniellim051000@gmail.com>
2021-05-09 15:33:12 +0800

    remove customer dashboard

--- dstr-assignment/Main.cpp
+++ dstr-assignment/Main.cpp
@@ -10,23 +10,19 @@ int main() {
 	do {
 		cout << "*** Bookstore System ***" << endl;
 		cout << "To perform an action, enter " << endl;
-		cout << "1 Customer Management" << endl;
-		cout << "2 Inventory Management" << endl;
-		cout << "3 Transaction Management" << endl;
-		cout << "4 to exit\n" << endl;
+		cout << "1 Inventory Management" << endl;
+		cout << "2 Transaction Management" << endl;
+		cout << "3 to exit\n" << endl;
 		cin >> choice;
 		switch (choice) {
 		case 1:
-			customerMenu();
-			break;
-		case 2:
 			inventoryMenu();
 			break;
-		case 3:
+		case 2:
 			showNewTransMenu();
 			break;
 		default:
 			cout << "Invalid selection.\n" << endl;
 		}
-	} while (choice != 4);
+	} while (choice != 3);
 }
